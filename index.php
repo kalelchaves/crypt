b81538d43d7cd91fdbff015bb58cd967a85b0d72
Pedro Escobar <pedro.escobar@live.com>
2015-11-08 18:17:06 -0200

    updated form

--- index.php
+++ index.php
@@ -104,7 +104,7 @@ function myCrypt($text){
 	$text = convertStringToAscii($text);
 	$text = convertArrayAsciiToStringCalculatedAscii( stringKeyCryptToOriginArrayAscii($text) );
 	$text .= str_pad(AUX_NUMBER_CALCULATE_CRYPT, 2,'0', STR_PAD_LEFT);
-	//$text = base64_encode($text);
+	$text = base64_encode($text);
 
 	return $text;
 
@@ -112,7 +112,7 @@ function myCrypt($text){
 
 function deCrypt($text){
 
-	//$text = base64_decode($text);
+	$text = base64_decode($text);
 	$numberCalculateCryptString = (int) getNumberCalculateToCryptString($text);
 	$text = removeNumberCalculateToCryptString($text);
 	$text = convertArrayCalculatedAsciiToStringAscii( stringKeyCryptToOriginArrayAscii($text), $numberCalculateCryptString );
@@ -126,25 +126,13 @@ function deCrypt($text){
 
 if($_POST){
 
-	if($_POST[palavra]){
-		
+	if($_POST[palavra]){		
 		$stringCrypt = myCrypt($_POST[palavra]);
-		//dump($stringCrypt);
 	}
 	
-	//$stringDeCrypt = deCrypt('MTQ3NjU0MzI2NTMyMTc1MDQxNjU3OTQyMzU2NzQzMDY1NDMyNjUzMjE3NTA0MTY1Nzk0MjM1Njc0MzA2NTQzMjY1MzIxNzUwNDE2NTc5NDIzNTY3NDM=');
-	//	dump($stringDeCrypt);
-
 	if($_POST[palavrac]){
 		$stringDeCrypt = deCrypt($_POST[palavrac]);
-		//dump($stringDeCrypt);
 	}
-	//$stringCrypt = myCrypt($_POST[palavra]);
-	//$stringDeCrypt = deCrypt($stringCrypt);
-
-	//dump($stringCrypt);
-	//dump($stringDeCrypt);
-
 }
 ?>
 
@@ -152,46 +140,43 @@ if($_POST){
 <!DOCTYPE html>
 <html>
 <head>
-	<!--Import Google Icon Font-->
 	<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<!--Import materialize.css-->
 	<link type="text/css" rel="stylesheet" href="./resources/css/materialize.min.css"  media="screen,projection"/>
-
-	<!--Let browser know website is optimized for mobile-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 
 <body>
 	<div class="container">
-		<div class="row">
-
-			<form class="col s6" method="post" accept-charset="ISO-8859-1">
-				<div class="row">
-					<div class="input-field col s6">
-						<input placeholder="" id="palavra" name="palavra" type="text" class="validate" value="<?php echo $stringDeCrypt;?>">
-						<label for="palavra">To Encrypt</label>
-					</div>
-				</div>			
-				<button class="btn waves-effect waves-light" type="submit" name="action">Encrypt
-					<i class="material-icons right">send</i>
-				</button>
-			</form>
-
-			<form class="col s12" method="post" accept-charset="ISO-8859-1">
-				<div class="row">
-					<div class="input-field col s12">
-						<input placeholder="" id="palavrac" name="palavrac" type="text" class="validate" value="<?php echo $stringCrypt;?>">
-						<label for="palavra">To Decrypt</label>
-					</div>
-				</div>			
-				<button class="btn waves-effect waves-light" type="submit" name="action">Decrypt
-					<i class="material-icons right">send</i>
-				</button>
-			</form>
-
+		<div class="card-panel hoverable">
+			<h3>Random Numbers</h3>
+			<div class="row">
+				<form class="col s12" method="post"  accept-charset="ISO-8859-1">
+					<div class="row">
+						<div class="input-field col s12">
+							<input placeholder="" id="palavra" name="palavra" type="text" class="validate" value="<?php echo $stringDeCrypt;?>">
+							<label for="palavra">To Encrypt</label>
+						</div>
+					</div>			
+					<button class="btn waves-effect waves-light" type="submit" name="action">Encrypt
+						<i class="material-icons right">visibility_off</i>
+					</button>
+				</form>
+				<form class="col s12" method="post" accept-charset="ISO-8859-1">
+					<div class="row">
+						<div class="input-field col s12">
+							<input placeholder="" id="palavrac" name="palavrac" type="text" class="validate" value="<?php echo $stringCrypt;?>">
+							<label for="palavra">To Decrypt</label>
+						</div>
+					</div>			
+					<button class="btn waves-effect waves-light" type="submit" name="action">Decrypt
+						<i class="material-icons right">visibility</i>
+					</button>
+				</form>
+			</div>
 		</div>
 	</div>
 	<script type="text/javascript" src="./resources/js/jquery-2.1.4.min.js"></script>
 	<script type="text/javascript" src="./resources/js/materialize.min.js"></script>
 </body>
 </html>
+
